f478f3d40bf280820a54e6508b76943382597cf5
Jakeom <dev.jakeom@gmail.com>
2021-07-25 00:55:56 -0400

    first commit7

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 var express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const { stringify } = require('querystring');
+var sender = require('emailsender');
 var router = express.Router();
 
 /* GET home page. */
@@ -9,5 +13,74 @@ router.get('/', function(req, res, next) {
 router.get('/about', (req,res,next)=>{
   res.render('about', {title : 'About', testPage : 'dasdfasdfasdfasdf'});
 })
+
+//app.post('/subscribe', async (req, res) => {
+router.post('/subscribe', async (req, res, next) => {
+
+  const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+  var isValidation = false;
+  // Check Email format
+  if (req.body.to.trim() == "" || req.body.to.match(regExp) == null) { 
+    isValidation = true;
+  }
+  if (req.body.text == null || req.body.text.trim() == "") { 
+    isValidation = true;
+  }
+
+  if(isValidation){
+    return res.json({ success: false, msg: 'Please, Check the Values.' });
+  }
+
+  if (!req.body.captcha)
+    return res.json({ success: false, msg: 'Please select captcha' });
+
+  // Secret key
+  const secretKey = '6Lc40LobAAAAABHXOBUj3K_bhgLOKvG0VJTAE4G9';
+
+  // Verify URL
+  const query = stringify({
+    secret: secretKey,
+    response: req.body.captcha,
+    remoteip: req.connection.remoteAddress
+  });
+
+  const verifyURL = "https://google.com/recaptcha/api/siteverify?"+query;
+
+  // Make a request to verifyURL
+  const body = await fetch(verifyURL).then(res => res.json());
+
+  // If not successful
+  if (body.success !== undefined && !body.success)
+    return res.json({ success: false, msg: 'Failed captcha verification' });
+
+
+  var mailConfig = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'dev.Jakeom@gmail.com',
+      pass: '!1djadudgus'
+    }
+  }
+  var _sender = new sender(mailConfig)
+
+  var mailOptions = {
+    from: 'dev.Jakeom@gmail.com',
+    to: req.body.to,
+    text: req.body.text
+  }
+
+  var callback = function(err, info){
+    //callback handle
+    //console.log(err);
+  }
+
+_sender.send(mailOptions, callback)
+
+  // If successful
+  return res.json({ success: true, msg: 'Eamil success!!' });
+});
 
 module.exports = router;
